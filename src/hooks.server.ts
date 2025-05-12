85b7b27bcfe9474708190731c4bf9e133cfa1723
momo391 <ia7636hs@gmail.com>
2025-05-12 23:35:11 +0100

    feat: add initial project configuration files and implement localization middleware

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,3 +1,4 @@
+import { paraglideMiddleware } from "$lib/paraglide/server";
 import { auth } from "$lib/server/auth";
 import { addMongoLogger, logger } from "$lib/server/logger";
 import { connectMongo, uri } from "$lib/server/mongo";
@@ -22,4 +23,21 @@ const authHandler: Handle = async ({ event, resolve }) => {
   return svelteKitHandler({ event, resolve, auth });
 };
 
-export const handle: Handle = sequence(startServer, authHandler);
+const paraglideHandler: Handle = ({ event, resolve }) =>
+  paraglideMiddleware(
+    event.request,
+    ({ request: localizedRequest, locale }) => {
+      event.request = localizedRequest;
+      return resolve(event, {
+        transformPageChunk: ({ html }) => {
+          return html.replace("%lang%", locale);
+        },
+      });
+    }
+  );
+
+export const handle: Handle = sequence(
+  startServer,
+  paraglideHandler,
+  authHandler
+);
