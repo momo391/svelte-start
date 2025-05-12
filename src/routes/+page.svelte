1f8d16eda9d253eb350cf5de7e49a1d78bf49acf
momo391 <ia7636hs@gmail.com>
2025-05-12 23:38:08 +0100

    feat: update welcome message to include additional technologies

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
 <div class="min-h-screen flex items-center justify-center">
-  <h1>Welcome to SvelteKit</h1>
+  <h1>
+    Welcome to SvelteKit & Docker & Mongo & Better&minus;Auth & MinIO & Winston
+    & Paraglide
+  </h1>
 </div>
