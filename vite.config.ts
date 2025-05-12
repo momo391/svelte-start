e5da6566c0cd2fb8b51bb3ccece515689c75532f
momo391 <ia7636hs@gmail.com>
2025-05-12 23:34:52 +0100

    feat: add paraglide plugin to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { paraglideVitePlugin } from "@inlang/paraglide-js";
+import tailwindcss from "@tailwindcss/vite";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+  plugins: [
+    paraglideVitePlugin({
+      project: "./project.inlang",
+      outdir: "./src/lib/paraglide",
+      strategy: ["cookie", "baseLocale"],
+    }),
+    tailwindcss(),
+    sveltekit(),
+  ],
 });
