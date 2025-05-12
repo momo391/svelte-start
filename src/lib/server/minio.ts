fed28854167664f63cdb55ff3d30b6ea885333f9
momo391 <ia7636hs@gmail.com>
2025-05-12 18:58:13 +0100

    fix: remove useSSL option from MinIO client configuration

--- src/lib/server/minio.ts
+++ src/lib/server/minio.ts
@@ -4,7 +4,6 @@ import * as Minio from "minio";
 const client = new Minio.Client({
   endPoint: "127.0.0.1",
   port: Number(env.MINIO_PORT),
-  useSSL: true,
   accessKey: env.MINIO_ACCESS_KEY,
   secretKey: env.MINIO_SECRET_KEY,
 });
